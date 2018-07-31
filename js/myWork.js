5d06b423faf8a0a7e78a36c9b68a5168444b1dbd
Robert Cortez <Nuhuhfoo@gmail.com>
2018-07-30 17:21:54 -0700

    initial commit

---
+++ js/myWork.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $(document).ready(function () {
+    $('.parallax').parallax();
+  
+    $(".dropdown-trigger").dropdown({
+      hover: false
+    });
+  
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+        
+  
+  });
+
---
+++ js/vlog.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+  
+    $(".dropdown-trigger").dropdown({
+      hover: false
+    });
+  
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+        
+  
+  });
