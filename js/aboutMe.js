45d690ca70ff835735c92709c5ba3ab57b33027c
Robert Cortez <Nuhuhfoo@gmail.com>
2018-12-04 15:44:04 -0800

    recreated floating navbar and small css updates

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $('.parallax').parallax();
-  
-    $(".dropdown-trigger").dropdown({
-      hover: false
-    });
-  
-    $(document).ready(function(){
-      $('.sidenav').sidenav();
-    });
-        
-  
   });
+
+  /* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
--- js/myWork.js
+++ js/myWork.js
@@ -5,18 +5,18 @@ $(document).ready(function(){
   $(document).ready(function () {
     $('.parallax').parallax();
   
-    $(".dropdown-trigger").dropdown({
-      hover: false
-    });
-  
-    $(document).ready(function(){
-      $('.sidenav').sidenav();
-    });
-        
-  
   });
 
   $(document).ready(function(){
     $('.collapsible').collapsible();
   });
 
+  /* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
